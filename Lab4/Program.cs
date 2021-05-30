474180c1f28bbd37e508247996d3a8045051de9b
Alrighter <59889888+Alrighter@users.noreply.github.com>
2021-05-30 18:59:26 +0300

    m

---
+++ Lab4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab3_4Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 42);
+
+            Matrix instance;
+
+            for (int i = 0; i < 26; i++)
+            {
+                instance = new Matrix(i * 3, true);
+                new Thread(instance.Move).Start();
+            }
+        }
+    }
+}
